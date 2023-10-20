233a9c1d326eeb51de1c11b45d8789e19280c3ab
Frans Kramer <franzzle@franzzle.com>
2023-10-20 14:07:37 +0200

    added docs

--- src/main/kotlin/com/franzzle/pixelart/svg/OpenAPIConfig.kt
+++ src/main/kotlin/com/franzzle/pixelart/svg/OpenAPIConfig.kt
@@ -5,6 +5,12 @@ import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+
+/**
+ * How to setup
+ * https://springdoc.org/migrating-from-springfox.html
+ * https://springdoc.org/index.html#how-can-i-hide-an-operation-or-a-controller-from-documentation
+ */
 @Configuration
 class OpenAPIConfig {
     @Bean
