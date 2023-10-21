c8e0d3b514072053964fa7f9acaa54a88163774b
franzzle <me@franzzle.com>
2023-10-21 20:05:42 +0200

    First almost working version

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+Based on the PHP code in this git repo :
+
+https://github.com/meyerweb/px2svg
+
+This service transform a pixelart PNG into a SVG that has the same blocky look.
+This is normally hard to do in even the most sophisticated vector applications.
+When converted to SVG it is possible to retain the crisp edges on the cluster 
+of pixel squares when scaling.
+
+PNG Input (binary, and a lot smaller)
+
+![secretagent.png](documentation%2Fimages%2Fsecretagent.png)
+
+SVG Output (can be scaled to any size)
+
+![secretagent.svg](documentation%2Fimages%2Fsecretagent.svg)
 
 
 Visit this for overview of Rest API
--- src/main/kotlin/com/franzzle/pixelart/svg/service/ConverterServiceImpl.kt
+++ src/main/kotlin/com/franzzle/pixelart/svg/service/ConverterServiceImpl.kt
@@ -1,34 +1,113 @@
 package com.franzzle.pixelart.svg.service
 
-import com.franzzle.pixelart.svg.Converter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.w3c.dom.Document
+import java.awt.image.BufferedImage
 import java.io.InputStream
+import java.util.*
+import java.util.stream.IntStream
 import javax.imageio.ImageIO
 
 @Service
 class ConverterServiceImpl(
-    @Autowired
-    private val anotherService: SvgService
+        @Autowired
+        private val svgService: SvgService
 ) : ConverterService {
 
     override fun convert(inputStream: InputStream?): String? {
         val bufferedImage = ImageIO.read(inputStream)
+        val svgDoc = svgService.createSvgDoc(bufferedImage.width, bufferedImage.height)
         System.out.println("Width : " + bufferedImage.width + " and Height : " + bufferedImage.height)
-        val createSvgDoc = anotherService.createSvgDoc(bufferedImage.width, bufferedImage.height)
-        return anotherService.serializeDocumentToString(createSvgDoc)
+        var svgh = generateSvgFromRaster(bufferedImage,svgDoc, DIRECTION_HORIZONTAL)
+        val svgv = generateSvgFromRaster(bufferedImage,svgDoc, DIRECTION_VERTICAL)
+        if (svgh.getElementsByTagName("rect").length < svgv.getElementsByTagName("rect").length) {
+            svgh = svgv
+        }
+
+
+        return svgService.serializeDocumentToString(svgh)
     }
 
-    private fun createRectElement(svg: Document, rgba: IntArray, x: Int, y: Int, width: Int, direction: Int) {
-        val rectWidth = if (direction == Converter.DIRECTION_VERTICAL) 1 else width
-        val rectHeight = if (direction == Converter.DIRECTION_VERTICAL) width else 1
+    private fun generateSvgFromRaster(image: BufferedImage,
+                                      svg: Document,
+                                      direction: Int): Document {
+        if (direction == DIRECTION_HORIZONTAL) {
+            IntStream.range(0, image.height).forEach { y: Int ->
+                var numberOfConsecutivePixels: Int
+                var x = 0
+                while (x < image.width) {
+                    numberOfConsecutivePixels = createLine(image, svg, x, y, direction)
+                    x += numberOfConsecutivePixels
+                }
+            }
+        } else {
+            IntStream.range(0, image.width).forEach { x: Int ->
+                var numberOfConsecutivePixels: Int
+                var y = 0
+                while (y < image.height) {
+                    numberOfConsecutivePixels = createLine(image, svg, x, y, direction)
+                    y += numberOfConsecutivePixels
+                }
+            }
+        }
+        return svg
+    }
+
+
+    private fun createLine(image: BufferedImage,
+                           svg: Document,
+                           x: Int,
+                           y: Int,
+                           direction: Int): Int {
+        val rgba = getPixelColors(image, x, y)
+        var delta = 1
+        while (isSimilarPixel(image, rgba, x, y, delta, direction)) {
+            delta++
+        }
+        createRectElement(svg, rgba, x, y, delta, direction)
+        return delta
+    }
+
+    private fun getPixelColors(image: BufferedImage,
+                               x: Int, y: Int): IntArray {
+        val pixel = image.getRGB(x, y)
+        val alpha = pixel shr 24 and 0xff
+        val red = pixel shr 16 and 0xff
+        val green = pixel shr 8 and 0xff
+        val blue = pixel and 0xff
+        return intArrayOf(red, green, blue, alpha)
+    }
+
+    private fun isSimilarPixel(image: BufferedImage,
+                               rgba: IntArray,
+                               x: Int,
+                               y: Int,
+                               delta: Int,
+                               direction: Int): Boolean {
+        return if (direction == DIRECTION_HORIZONTAL) {
+            val res = x + delta
+            res < image.width && Objects.deepEquals(rgba, getPixelColors(image, res, y))
+        } else {
+            val res = y + delta
+            res < image.height && Objects.deepEquals(rgba, getPixelColors(image, x, res))
+        }
+    }
+
+    private fun createRectElement(svg: Document,
+                                  rgba: IntArray,
+                                  x: Int,
+                                  y: Int,
+                                  width: Int,
+                                  direction: Int) {
+        val rectWidth = if (direction == DIRECTION_VERTICAL) 1 else width
+        val rectHeight = if (direction == DIRECTION_VERTICAL) width else 1
         val rect = svg.createElement("rect")
-        rect.setAttribute("x", Integer.toString(x))
-        rect.setAttribute("y", Integer.toString(y))
-        rect.setAttribute("width", Integer.toString(rectWidth))
-        rect.setAttribute("height", Integer.toString(rectHeight))
-        rect.setAttribute("fill", "rgb(" + rgba[0] + "," + rgba[1] + "," + rgba[2] + ")")
+        rect.setAttribute("x", x.toString())
+        rect.setAttribute("y", y.toString())
+        rect.setAttribute("width", rectWidth.toString())
+        rect.setAttribute("height", rectHeight.toString())
+        rect.setAttribute("fill", "rgb(" + rgba[0] + "," + rgba[1] + "," + rgba[2] + "," + rgba[3] + ")")
         val alpha = rgba[3]
         if (alpha > 0) {
             rect.setAttribute("fill-opacity", ((128 - alpha) / 128.0).toString())
@@ -36,4 +115,8 @@ class ConverterServiceImpl(
         svg.documentElement.appendChild(rect)
     }
 
+    companion object {
+        const val DIRECTION_HORIZONTAL = 1
+        const val DIRECTION_VERTICAL = 2
+    }
 }
